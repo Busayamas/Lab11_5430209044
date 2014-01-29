09e983db5ae165b422059803b107c65d820c3421
tippfiiieez <tippfiieez@gmail.com>
2014-01-29 14:51:21 +0700

    Edited by Tippawan Mongnoi

--- Lab11_1.php
+++ Lab11_1.php
@@ -1,6 +1,7 @@
-<?php 
-	phpinfo(); 
-	echo '<p>Hello World</p>';
-	echo '<p>What a wonderful day</p>';
-	echo '<p>We are having fun</p>';
-?>
+<? php
+
+phpinfo(;
+
+echo '<p>My name is Tippawan mongnoi </p>';
+
+?> 
